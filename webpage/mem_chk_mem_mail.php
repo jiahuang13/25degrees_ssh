eec47b53defc4cae6ad5745ea2c2bece6e2d811a
jiahuang <graceooh@icloud.com>
2023-11-30 20:00:58 +0800

    用戶註冊功能

--- webpage/mem_chk_mem_mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('../shared/class_conn.php');
-include_once('../shared/class_mem.php');
-$mem = new Mem;
-
-$mem_mail = $_POST['mem_mail'];
-
-$mem->chkMemMail($mem_mail);
-echo $mem->total_rows;
