3738d23ae15c15ac92912fbcddc045b7e4a636ba
jiahuang <graceooh@icloud.com>
2023-12-02 16:52:00 +0800

    添加axios請求、響應攔截器

--- assist/common.js
+++ assist/common.js
@@ -43,3 +43,51 @@ function logout() {
     document.querySelector('.uname-render').innerText = "登入/註冊"
   })
 }
+
+// 添加请求拦截器
+// 统一携带token
+axios.interceptors.request.use(function (config) {
+  // 可以通过headers，查看+设置请求头
+  // config.headers['info'] = 'itheima666'
+  // 每次发送请求，都会执行这个回调函数
+  // console.log(config)
+  // 在发送请求之前做些什么,比如: 统一设置token
+  const token = localStorage.getItem('token')
+  // token存在，才携带
+  if (token) {
+    config.headers['Authorization'] = token
+  }
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+
+// 添加响应拦截器
+// 统一处理token过期
+// 数据剥离
+axios.interceptors.response.use(function (response) {
+  // console.log(response)
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么，比如: 数据剥离
+  // 剥离data属性，页面中少写.data属性，直接可以获取到数据
+  return response.data;
+}, function (error) {
+  // console.dir(error)
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么: 比如统一处理token失效
+  // 统一处理token失效
+  if (error.response.status === 401) {
+    // 弹框提示用户
+    showToast('请重新登录')
+    // 删除缓存
+    localStorage.removeItem('token')
+    localStorage.removeItem('username')
+    // 返回登录页
+    setTimeout(() => {
+      location.href = './login.html'
+    }, 1500)
+  }
+  return Promise.reject(error);
+});
